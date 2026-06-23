d80d7fedcb5917db2f121246f96cf1d6d753c614
Hyunsik-Jo20 <Hyunsik-Jo20@users.noreply.github.com>
2026-06-23 09:23:41 +0900

    Validate daily inputs before showing missing data

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "ongirok-v6";
+const CACHE = "ongirok-v7";
 const ASSETS = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", event => {
